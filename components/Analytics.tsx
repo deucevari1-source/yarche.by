74add2fdbedfdf973d9a0458542dc4ffda4a77a5
Влад <deuce.vari1@gmail.com>
2026-05-21 19:53:02 +0300

    analytics: restore explicit referrer/url in ym init to match Metrika snippet

--- components/Analytics.tsx
+++ components/Analytics.tsx
@@ -47,7 +47,7 @@ export function AnalyticsScripts() {
             for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
             k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
             (window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=${METRIKA_ID}', 'ym');
-            ym(${METRIKA_ID}, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+            ym(${METRIKA_ID}, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
           `,
         }}
       />
